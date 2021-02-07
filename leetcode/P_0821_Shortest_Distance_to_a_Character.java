1b2bf4b34b34cf326adc013ec21dc771d192b15a
abhishek <mailabhipowar@gmail.com>
2021-02-07 20:04:40 +0530

    orignal solution added

--- leetcode/P_0821_Shortest_Distance_to_a_Character.java
+++ leetcode/P_0821_Shortest_Distance_to_a_Character.java
@@ -5,33 +5,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class P_0821_Shortest_Distance_to_a_Character {
-	// public int[] shortestToChar(String s, char c) {
-	// int[] res = new int[s.length()];
-	// List<Integer> found = new ArrayList<Integer>();
-	// for (int i = 0; i < s.length(); i++) {
-	// if (s.charAt(i) == c) {
-	// found.add(i);
-	// }
-	// }
-	// found.add(Integer.MAX_VALUE / 2);
-	// int prev = Integer.MIN_VALUE / 2;
-	// int next = found.get(0);
-	// int foundIdx = 0;
-	// for (int i = 0; i < res.length; i++) {
-	// res[i] = Math.min(i - prev, next - i);
-	//
-	// if (i >= next) {
-	// foundIdx++;
-	// prev = next;
-	// if (foundIdx < found.size()) {
-	// next = found.get(foundIdx);
-	// }
-	// }
-	//
-	// }
-	//
-	// return res;
-	// }
+	public int[] shortestToCharOG(String s, char c) {
+		int[] res = new int[s.length()];
+		List<Integer> found = new ArrayList<Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == c) {
+				found.add(i);
+			}
+		}
+		found.add(Integer.MAX_VALUE / 2);
+		int prev = Integer.MIN_VALUE / 2;
+		int next = found.get(0);
+		int foundIdx = 0;
+		for (int i = 0; i < res.length; i++) {
+			res[i] = Math.min(i - prev, next - i);
+
+			if (i >= next) {
+				foundIdx++;
+				prev = next;
+				if (foundIdx < found.size()) {
+					next = found.get(foundIdx);
+				}
+			}
+
+		}
+
+		return res;
+	}
+
 	public int[] shortestToChar(String s, char c) {
 		int[] res = new int[s.length()];
 		int prev = Integer.MIN_VALUE / 2;
@@ -39,15 +40,15 @@ public class P_0821_Shortest_Distance_to_a_Character {
 			if (s.charAt(i) == c) {
 				prev = i;
 			}
-			res[i]=i-prev;
-			
+			res[i] = i - prev;
+
 		}
-		prev=Integer.MAX_VALUE/2;
-		for (int i = s.length()-1; i>=0; i--) {
-			if (s.charAt(i)==c) {
-				prev=i;
+		prev = Integer.MAX_VALUE / 2;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (s.charAt(i) == c) {
+				prev = i;
 			}
-			res[i]=Math.min(res[i],prev-i );
+			res[i] = Math.min(res[i], prev - i);
 		}
 
 		return res;

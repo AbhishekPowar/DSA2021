52c324c8e04a16357fb6a4caecbd91e1db179c9c
abhishek <mailabhipowar@gmail.com>
2021-02-24 21:00:39 +0530

    print object function(s)

--- TestRunner/Output.java
+++ TestRunner/Output.java
@@ -1,6 +1,7 @@
 package TestRunner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Output<T> {
@@ -18,5 +19,46 @@ public class Output<T> {
 
 	public void add(T ar) {
 		outArgs.add(ar);
+	}
+
+	public String print(int i) {
+		
+		StringBuilder sb = new StringBuilder("");
+		Object obj = this.outArgs.get(i);
+		
+		if (obj.getClass().isArray()) {
+			sb.append(printArray(obj));
+		} else {
+			sb.append(obj.toString());
+		}
+		
+		return sb.toString();
+	}
+	public String printData(Object i) {
+
+		StringBuilder sb = new StringBuilder("");
+		Object obj = i;
+
+		if (obj.getClass().isArray()) {
+			sb.append(printArray(obj));
+		} else {
+			sb.append(obj.toString());
+		}
+
+		return sb.toString();
+	}
+
+	public String printArray(Object o) {
+		if (o.getClass() == String[].class) {
+			return Arrays.toString((String[]) o);
+		}
+		if (o.getClass() == Integer[].class) {
+			return Arrays.toString((Integer[]) o);
+		}
+		if (o.getClass() == int[].class) {
+			return Arrays.toString((int[]) o);
+		}
+
+		return "error";
 	}
 }
